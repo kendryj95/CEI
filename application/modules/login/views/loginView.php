ae5e74f12d44ef7418f71f4c140c5331a64be94d
Ing. Kendry Ortiz <kendry.ortiz@insignia.com.ve>
2017-11-30 14:03:31 -0400

    Cambio al titulo de la web en el modulo de login "Panel" por "Plataforma"

--- application/modules/login/views/loginView.php
+++ application/modules/login/views/loginView.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Panel | CEI</title>
+    <title>Plataforma | CEI</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
@@ -23,7 +23,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <b>Panel </b>CEI
+    <b>Plataforma </b>CEI
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
