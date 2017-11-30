54b90164f8ff2fcf00a784467b39f4f57d4123f5
Ing. Kendry Ortiz <kendry.ortiz@insignia.com.ve>
2017-11-30 13:58:20 -0400

    Cambio al titulo de la web "Panel" por "Plataforma"

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Panel | CEI</title>
+    <title>Plataforma | CEI</title>
         <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
@@ -47,7 +47,7 @@
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>CEI</b></span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Panel</b> CEI</span>
+            <span class="logo-lg"><b>Plataforma</b> CEI</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
